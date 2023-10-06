61ea928476018dbcdbc7746f3e93e0688d32701a
Nicolas Gallardo <nicolasgallardo1507@gmail.com>
2023-10-06 09:36:06 -0300

    ProbandoTemplates

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from .views import*
 
+urlpatterns = [
+    path('pagina/',PruebaPagina,name="index"),
+    path('inicio/',inicio,name="inicio")
+]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def PruebaPagina(request):
+    return render(request,"AppBlog/base.html")
 
+def inicio(request):
+    return render(request, "AppBlog/inicio.html")
